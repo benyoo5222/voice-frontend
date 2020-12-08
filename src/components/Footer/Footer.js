887ef6ed075704da14844d6ee82b44fe1a1281a9
benyoo5222 <benyoo5222@gmail.com>
2020-12-07 21:49:56 -0500

    Added pointer events to icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,73 +5,87 @@ import {
     Link,
     useRouteMatch,
     useParams,
-    useLocation
+    useLocation,
+    useHistory,
   } from "react-router-dom";
 import './Footer.scss';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Footer({userInfo}) {
     console.log('Use location', useLocation());
     const location = useLocation();
-    return (   
-            <div className="footer-container">
-                
-                <div className="icons-container">
+    const history = useHistory();
 
-                    <div className="footer-twitter-icon-container">
-                        <div className="image-container footer-twitter-container">
+    const test = () => {
+        history.push('/');
+    };
+    return (   
+        <div className="footer-container">
+            
+            <div className="icons-container">
+                <div className="footer-twitter-icon-container">
+                    <div className="image-container footer-twitter-container">
+                        <Link to="/">
                             <svg viewBox="0 0 24 24" className="twitter-logo">
                                 <path className="main-color" d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
                             </svg>
-                        </div>
+                        </Link>
                     </div>
-                    
-                    <div className="footer-icon-container">
-                        <div className="footer-icon-sizing">
+                </div>
+                
+                <div className="footer-icon-container">
+                    <div className="footer-icon-sizing">
+                        <Link to="/">
                             <svg viewBox="0 0 24 24" className={"footer-icon " 
                                 + `${location.pathname === '/' || location.pathname === '/home' ? "active" : ""}`}>
                                 <g>
                                     <path d="M22.58 7.35L12.475 1.897c-.297-.16-.654-.16-.95 0L1.425 7.35c-.486.264-.667.87-.405 1.356.18.335.525.525.88.525.16 0 .324-.038.475-.12l.734-.396 1.59 11.25c.216 1.214 1.31 2.062 2.66 2.062h9.282c1.35 0 2.444-.848 2.662-2.088l1.588-11.225.737.398c.485.263 1.092.082 1.354-.404.263-.486.08-1.093-.404-1.355zM12 15.435c-1.795 0-3.25-1.455-3.25-3.25s1.455-3.25 3.25-3.25 3.25 1.455 3.25 3.25-1.455 3.25-3.25 3.25z"></path>
                                 </g>
                             </svg>
-                        </div>
-
+                        </Link>
+                    </div>
+                    
+                    <Link to="/">
                         <div className={"footer-icon-text " 
                                 + `${location.pathname === '/' || location.pathname === '/home' ? "active" : ""}`}>
                             Home
                         </div>
+                    </Link>
+                </div>
+                
+                <div className="footer-icon-container footer-search-icon">
+                    <div className="footer-icon-sizing">
+                        <svg viewBox="0 0 24 24" className="footer-icon">
+                            <g>
+                                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+                                </path>
+                            </g>
+                        </svg>
                     </div>
-                    
-                    <div className="footer-icon-container footer-search-icon">
-                        <div className="footer-icon-sizing">
-                            <svg viewBox="0 0 24 24" className="footer-icon">
-                                <g>
-                                    <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
-                                    </path>
-                                </g>
-                            </svg>
-                        </div>
-                    </div>
+                </div>
 
-                    <div className="footer-icon-container">
-                        <div className="footer-icon-sizing">
+                <div className="footer-icon-container">
+                    <div className="footer-icon-sizing">
+                        <Link to="/notification">
                             <svg viewBox="0 0 24 24" className={"footer-icon " 
                                     + `${location.pathname === '/notification' ? "active" : ""}`}>
                                 <path d="M21.697 16.468c-.02-.016-2.14-1.64-2.103-6.03.02-2.532-.812-4.782-2.347-6.335C15.872 2.71 14.01 1.94 12.005 1.93h-.013c-2.004.01-3.866.78-5.242 2.174-1.534 1.553-2.368 3.802-2.346 6.334.037 4.33-2.02 5.967-2.102 6.03-.26.193-.366.53-.265.838.102.308.39.515.712.515h4.92c.102 2.31 1.997 4.16 4.33 4.16s4.226-1.85 4.327-4.16h4.922c.322 0 .61-.206.71-.514.103-.307-.003-.645-.263-.838zM12 20.478c-1.505 0-2.73-1.177-2.828-2.658h5.656c-.1 1.48-1.323 2.66-2.828 2.66zM4.38 16.32c.74-1.132 1.548-3.028 1.524-5.896-.018-2.16.644-3.982 1.913-5.267C8.91 4.05 10.397 3.437 12 3.43c1.603.008 3.087.62 4.18 1.728 1.27 1.285 1.933 3.106 1.915 5.267-.024 2.868.785 4.765 1.525 5.896H4.38z">
 
                                 </path>
                             </svg>
-                        </div>
+                        </Link>
+                    </div>
 
+                    <Link to="/notification">
                         <div className={"footer-icon-text " 
                                 + `${location.pathname === '/notification' ? "active" : ""}`}>
                             Notifications
                         </div>
-                    </div>
+                    </Link>
+                </div>
 
-                    <div className="footer-icon-container">
-                        <div className="footer-icon-sizing">
+                <div className="footer-icon-container">
+                    <div className="footer-icon-sizing">
+                        <Link to="/messages">
                             <svg viewBox="0 0 24 24" className={"footer-icon " 
                                         + `${location.pathname === '/messages' ? "active" : ""}`}>
                                 <g>
@@ -80,33 +94,36 @@ function Footer({userInfo}) {
                                     </path>
                                 </g>
                             </svg>
-                        </div>
+                        </Link>
+                    </div>
 
+                    <Link to="/messages">
                         <div className={"footer-icon-text " 
                                 + `${location.pathname === '/messages' ? "active" : ""}`}>
                             Messages
                         </div>
+                    </Link>
+                </div>
+            
+                <div className="compose-button-container">
+                    <div className="compose-button">
+                        <svg viewBox="0 0 24 24" className="compose-icon">
+                            <g>
+                                <path d="M8.8 7.2H5.6V3.9c0-.4-.3-.8-.8-.8s-.7.4-.7.8v3.3H.8c-.4 0-.8.3-.8.8s.3.8.8.8h3.3v3.3c0 .4.3.8.8.8s.8-.3.8-.8V8.7H9c.4 0 .8-.3.8-.8s-.5-.7-1-.7zm15-4.9v-.1h-.1c-.1 0-9.2 1.2-14.4 11.7-3.8 7.6-3.6 9.9-3.3 9.9.3.1 3.4-6.5 6.7-9.2 5.2-1.1 6.6-3.6 6.6-3.6s-1.5.2-2.1.2c-.8 0-1.4-.2-1.7-.3 1.3-1.2 2.4-1.5 3.5-1.7.9-.2 1.8-.4 3-1.2 2.2-1.6 1.9-5.5 1.8-5.7z"></path>
+                            </g>
+                        </svg>
                     </div>
-                
-                    <div className="compose-button-container">
-                        <div className="compose-button">
-                            <svg viewBox="0 0 24 24" className="compose-icon">
-                                <g>
-                                    <path d="M8.8 7.2H5.6V3.9c0-.4-.3-.8-.8-.8s-.7.4-.7.8v3.3H.8c-.4 0-.8.3-.8.8s.3.8.8.8h3.3v3.3c0 .4.3.8.8.8s.8-.3.8-.8V8.7H9c.4 0 .8-.3.8-.8s-.5-.7-1-.7zm15-4.9v-.1h-.1c-.1 0-9.2 1.2-14.4 11.7-3.8 7.6-3.6 9.9-3.3 9.9.3.1 3.4-6.5 6.7-9.2 5.2-1.1 6.6-3.6 6.6-3.6s-1.5.2-2.1.2c-.8 0-1.4-.2-1.7-.3 1.3-1.2 2.4-1.5 3.5-1.7.9-.2 1.8-.4 3-1.2 2.2-1.6 1.9-5.5 1.8-5.7z"></path>
-                                </g>
-                            </svg>
-                        </div>
 
-                        <div className="large-compose-button">
-                            <span className="large-compose-button-text">Tweet</span>
-                        </div>
+                    <div className="large-compose-button">
+                        <span className="large-compose-button-text">Tweet</span>
                     </div>
                 </div>
-                
-                <div className="image-container footer-profile">
-                    <img src={userInfo ? userInfo.profileImageUrl : null} className="profile-picture pic-border footer-pic" />
-                </div>
-            </div>        
+            </div>
+            
+            <div className="image-container footer-profile">
+                <img src={userInfo ? userInfo.profileImageUrl : null} className="profile-picture pic-border footer-pic" />
+            </div>
+        </div>        
     );
 };
 
---
+++ src/components/TweetModal/TweetModal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams,
+  } from "react-router-dom";
+
+function TweetModal() {
+   
+    return (
+        <div></div>
+    );
+};
+
+export default TweetModal;
